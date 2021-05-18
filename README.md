f3ab3eba42cde9d74f62635f39489707b5c20c8d
seegler <seegler@gmail.com>
2021-05-18 21:59:32 +0530

    Add logout and login pages

---
+++ README.md
@@ -0,0 +1,9 @@
+* User login
+
+```
+user: summarrain
+email: summerrain@myproject.com
+password: a^u4M3q!
+```
+
+* Admin login
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -18,9 +18,12 @@ from django.conf.urls import url
 from django.urls import path
 from boards import views
 from accounts import views as accounts_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^signup/$', accounts_views.signup, name='signup'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
     url(r'^boards/$', views.home, name='home'),
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
