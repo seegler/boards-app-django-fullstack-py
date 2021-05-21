42a525a8b69c80eb0d60ecd87c9280f41b4998a4
seegler <seegler@gmail.com>
2021-05-21 23:21:20 +0530

    redirect user to last post after replying to a topic

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.contrib.auth.models import User
 from django.db.models import Count
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import UpdateView, ListView
 from django.utils import timezone
@@ -78,7 +78,15 @@ def reply_topic(request, pk, topic_pk):
             post.created_by = request.user
             topic.last_updated = timezone.now()
             post.save()
-            return redirect('topic_posts', pk=pk, topic_pk=topic_pk)
+
+            topic_url = reverse('topic_posts', kwargs={'pk': pk, 'topic_pk': topic_pk})
+            topic_post_url = '{url}?page={page}#{id}'.format(
+                url=topic_url,
+                id=post.pk,
+                page=topic.get_page_count()
+            )
+
+            return redirect(topic_post_url)
     else:
         form = PostForm()
     return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
