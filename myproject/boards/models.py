85b6ea683fbfe0baa5c514412155a78ff49fe091
seegler <seegler@gmail.com>
2021-05-21 21:06:29 +0530

    Accept markdown text in topic post message

--- myproject/boards/models.py
+++ myproject/boards/models.py
@@ -4,6 +4,8 @@ from django.utils.text import Truncator
 # Create your models here.
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.html import mark_safe
+from markdown import markdown
 
 
 class Board(models.Model):
@@ -42,4 +44,7 @@ class Post(models.Model):
     def __str__(self):
         truncated_message = Truncator(self.message)
         return truncated_message.chars(30)
+
+    def get_message_as_markdown(self):
+        return mark_safe(markdown(self.message, safe_mode='escape'))
 
