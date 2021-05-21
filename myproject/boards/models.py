885c5dee3b206eca53d1f5b16d61b611abc20df9
seegler <seegler@gmail.com>
2021-05-21 17:15:54 +0530

    reply topic page

--- myproject/boards/models.py
+++ myproject/boards/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import Truncator
 
 # Create your models here.
 from django.db import models
@@ -19,6 +20,9 @@ class Topic(models.Model):
     board = models.ForeignKey(Board, related_name='topics',  on_delete=models.CASCADE)
     starter = models.ForeignKey(User, related_name='topics',  on_delete=models.DO_NOTHING)
 
+    def __str__(self):
+        return self.subject
+
 
 class Post(models.Model):
     message = models.TextField(max_length=4000)
@@ -27,3 +31,13 @@ class Post(models.Model):
     updated_at = models.DateTimeField(null=True)
     created_by = models.ForeignKey(User, related_name='posts', on_delete=models.DO_NOTHING)
     updated_by = models.ForeignKey(User, null=True, related_name='+', on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        truncated_message = Truncator(self.message)
+        return truncated_message.chars(30)
+
+    def get_posts_count(self):
+        return Post.objects.filter(topic__board=self).count()
+
+    def get_last_post(self):
+        return Post.objects.filter(topic__board=self).order_by('-created_at').first()
