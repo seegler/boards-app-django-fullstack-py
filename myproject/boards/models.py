5ef61175b3b8746bc7aafd6001e50c2d71ecc038
seegler <seegler@gmail.com>
2021-05-21 17:48:08 +0530

    Fix board model to return topic count and last post

--- myproject/boards/models.py
+++ myproject/boards/models.py
@@ -13,6 +13,12 @@ class Board(models.Model):
     def __str__(self):
         return self.name
 
+    def get_posts_count(self):
+        return Post.objects.filter(topic__board=self).count()
+
+    def get_last_post(self):
+        return Post.objects.filter(topic__board=self).order_by('-created_at').first()
+
 
 class Topic(models.Model):
     subject = models.CharField(max_length=255)
@@ -36,8 +42,3 @@ class Post(models.Model):
         truncated_message = Truncator(self.message)
         return truncated_message.chars(30)
 
-    def get_posts_count(self):
-        return Post.objects.filter(topic__board=self).count()
-
-    def get_last_post(self):
-        return Post.objects.filter(topic__board=self).order_by('-created_at').first()
