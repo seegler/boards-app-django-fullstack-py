06e3743868de8e002dff2c8ee828666257e99f43
seegler <seegler@gmail.com>
2021-05-22 22:10:38 +0530

    Add STATIC_ROOT in settings

--- django-boards/django-boards/settings.py
+++ django-boards/django-boards/settings.py
@@ -134,6 +134,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+STATIC_ROOT = '/env/apps/django_boards/static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
