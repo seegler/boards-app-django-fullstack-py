b042680d4328f86b73284f9c703024aed04ba002
seegler <seegler@gmail.com>
2021-05-22 23:02:51 +0530

    Fix typo in STATIC_ROOT

--- django-boards/django-boards/settings.py
+++ django-boards/django-boards/settings.py
@@ -135,7 +135,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_ROOT = '/env/apps/django_boards/static'
+STATIC_ROOT = '/env/apps/django-boards/static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
